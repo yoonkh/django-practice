f49b5d133bd751c71769450c12c5f1abd89ee331
yoonkh <zizou0812@gmail.com>
2017-05-29 20:43:55 +0900

    add all

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.utils import timezone
 from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    # posts 변수에 ORM을 이용해서 Post의 리스트 ( 쿼리셋 )를 대입
+    post = Post.objects.filter(published_date__lte=timezone.now())
+
+
+    context = {
+        'title': 'Postlist from post_list view',
+        'posts': post,
+    }
+    # return HttpResponse('<html><body>Post List</body></html>')
+    return render(request, 'blog/post_list.html', context=context)
 # Create your views here.
